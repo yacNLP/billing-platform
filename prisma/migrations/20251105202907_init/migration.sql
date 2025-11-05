94d47757d67421bbc96aa7d25428e2e88c0b94d2
yhaddad <yacinehaddad.ut2j@gmail.com>
2025-11-05 21:31:04 +0100

    feat(prisma): add updatedAt on Customer

--- prisma/migrations/20251105202907_init/migration.sql
+++ prisma/migrations/20251105202907_init/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "Customer" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
 );
