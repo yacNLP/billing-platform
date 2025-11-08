1c98149ed1688062b078267195f8ce26923f37d6
yhaddad <yacinehaddad.ut2j@gmail.com>
2025-11-08 18:15:34 +0100

    refactor(customers): add CustomersModule and align structure with ProductsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { PrismaService } from './prisma.service';
-import { CustomersController } from './customers/customers.controller';
-import { CustomersService } from './customers/customers.service';
 import { ProductsModule } from './products/products.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController, CustomersController],
-  providers: [PrismaService, CustomersService],
+  imports: [CustomersModule, ProductsModule],
+  controllers: [AppController],
 })
 export class AppModule {}
