ea6bef1368e7e7a7fb96c5d8c066574c54e9105c
yhaddad <yacinehaddad.ut2j@gmail.com>
2025-11-09 21:17:40 +0100

    docs(api): enable Swagger to product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,7 +13,9 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly service: ProductsService) {}
