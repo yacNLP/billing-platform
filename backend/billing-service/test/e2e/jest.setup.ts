5b75809ce5023507f86174c755efe8f0960e6b88
yhaddad <yacinehaddad.ut2j@gmail.com>
2026-02-04 18:30:15 +0100

    test(e2e): update jest setup with migration

--- backend/billing-service/test/e2e/jest.setup.ts
+++ backend/billing-service/test/e2e/jest.setup.ts
@@ -1,6 +1,12 @@
+import { execSync } from 'child_process';
 import { seedTestData, disconnectPrisma } from './seed';
 
 beforeAll(async () => {
+  // apply prisma migrations to test database
+  execSync('npx prisma migrate deploy', {
+    stdio: 'inherit',
+  });
+
   await seedTestData();
 });
 
